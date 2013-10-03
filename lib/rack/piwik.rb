ffa4b29d08195009f304a2207682aebfe67d4071
Daniel Stutzman <dtstutz@gmail.com>
2013-10-02 19:11:43 -0600

    Un-hardcoded URL of piwik server.

--- lib/rack/piwik.rb
+++ lib/rack/piwik.rb
@@ -9,7 +9,7 @@ module Rack #:nodoc:
         response.each { |part| body << part }
         index = body.rindex("</body>")
         if index
-          body.insert(index, tracking_code(options[:web_property_id]))
+          body.insert(index, tracking_code(options[:piwik_url]))
           headers["Content-Length"] = body.length.to_s
           response = [body]
         end
@@ -22,21 +22,21 @@ module Rack #:nodoc:
 
       # Returns JS to be embeded. This takes one argument, a Web Property ID
       # (aka UA number).
-      def tracking_code(web_property_id)
+      def tracking_code(piwik_url)
         returning_value = <<-EOF
 <script type="text/javascript">
   var _paq = _paq || [];
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://162.243.28.101/analytics//";
+    var u="#{piwik_url}";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 1]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
     g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="http://162.243.28.101/analytics/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+<noscript><p><img src="#{piwik_url}piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
 EOF
       returning_value
       end
